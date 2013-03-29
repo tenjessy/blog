7951cf0c99d4d5688f91597ccadfd4c29d09b782
jessy <tenjessy@gmail.com>
2013-03-29 11:57:31 +0800

    test

--- git.sh
+++ git.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 
-gitdir=`pwd`
-cd $gitdir
+gitdir='/Jessy_Project/GitWorkspace/blog'
 echo '输入commit messages:'
 read gitmsg
+cd $gitdir
 git add .
 git commit -m "$gitmsg"
 git push origin gh-pages
